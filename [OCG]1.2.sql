b9e1a9234614b0644cf74921f64d795709e28d8c
le viet nam <65153524+nambeo1998@users.noreply.github.com>
2022-10-08 16:00:13 +0700

    Update [OCG]1.2.sql

--- [OCG]1.2.sql
+++ [OCG]1.2.sql
@@ -1,3 +1,4 @@
-SELECT a.* FROM ocg.Customer a
-left join ocg.`Purchase Order` b on b.Customer_Id = a.id
-where b.Product_Id != 2 or b.product_Id is null
+SELECT a.Id FROM ocg.Customer a
+left join ocg.`Purchase Order` b on b.Customer_Id = a.Id 
+where a.Id not in 
+(select Customer_Id from ocg.`Purchase Order` where Product_Id = 2 or null)
